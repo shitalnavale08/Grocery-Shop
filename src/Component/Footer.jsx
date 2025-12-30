68a96f6056ac275497c4dedea55d1c7e37d24d3d
sonubhosle <sbhosle1011@gmail.com>
2025-12-30 15:29:21 +0530

    Footer Changed

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laborum facere deserunt! Eum inventore alias quod deleniti sequi libero fugit.</div>
   )
 }
 
